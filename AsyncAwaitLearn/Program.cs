6dfbdc04d4511c3fddeb5a38b77c5f82cde52a04
David Zuñiga <dczunigap@outlook.com>
2020-11-13 13:03:33 -0600

    Update Program.cs
    Prueba con GithubActions

--- AsyncAwaitLearn/Program.cs
+++ AsyncAwaitLearn/Program.cs
@@ -6,7 +6,7 @@ namespace AsyncAwaitLearn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! GitHub actions");
         }
     }
 }
